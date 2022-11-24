47be1a43257b04ea1e3f9f05bc15b8dfbacf0a2b
misha <mishaivanov99@gmail.com>
2022-11-24 22:56:00 +0300

    added docker compose support added mysql using via aiomysql driver

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,8 @@ COPY ./app/pyproject.toml ./app/poetry.lock* /app/
 ARG INSTALL_DEV=false
 RUN bash -c "if [ $INSTALL_DEV == 'true' ] ; then poetry install --no-root ; else poetry install --no-root --only main ; fi"
 
-COPY ./app /app
+COPY ./app /app/
+COPY ["./.env", "/app/"]
 ENV PYTHONPATH=/app
 EXPOSE 8000
 
--- app/main.py
+++ app/main.py
@@ -1,25 +1,53 @@
+import datetime
 import logging
-
-from fastapi import FastAPI
 from multiprocessing import Manager
 
+from dotenv import dotenv_values
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from starlette.middleware.cors import CORSMiddleware
 
+config = dotenv_values(".env")
+engine = create_async_engine(
+    f"mysql+aiomysql://{config['MYSQL_USER']}:{config['MYSQL_PASSWORD']}@{config['MYSQL_HOST']}/{config['MYSQL_DATABASE']}")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 manager = Manager()
 store = manager.dict()
-store["count"] = 0
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
-async def root():
-    return store["count"]
+async def root(request: Request):
+    date = datetime.datetime.now().isoformat()
+    user_agent = request.headers.get('User-Agent')
+    async with SessionLocal() as session:
+        await session.execute(
+            f"INSERT INTO counters(datetime, client_info, value) VALUES ('{date}','{user_agent}',{store['count']})")
+        await session.commit()
+    return store['count']
 
 
 @app.get("/stat")
-async def counter():
+async def counter(request: Request):
     store["count"] = store["count"] + 1
+    date = datetime.datetime.now().isoformat()
+    user_agent = request.headers.get('User-Agent')
+    async with SessionLocal() as session:
+        await session.execute(
+            f"INSERT INTO counters(datetime, client_info, value) VALUES ('{date}','{user_agent}',{store['count']})")
+        await session.commit()
+
     return store["count"]
 
 
@@ -40,3 +68,11 @@ async def startup_event():
         )
     )
     uv_logger.addHandler(handler)
+    async with SessionLocal() as session:
+        res = await session.execute(
+            f"SELECT value FROM counters ORDER BY datetime DESC LIMIT 1")
+        count = res.scalar()
+        if not count:
+            store["count"] = 0
+        else:
+            store["count"] = count
