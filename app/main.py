b426d36382f1352785fb202631e6021c0c7e3f63
misha <mishaivanov99@gmail.com>
2022-11-25 03:30:02 +0300

    update

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+"""
+Основной файл программы
+"""
+
 import datetime
 import logging
 from multiprocessing import Manager
@@ -11,7 +15,8 @@ from starlette.middleware.cors import CORSMiddleware
 
 config = dotenv_values(".env")
 engine = create_async_engine(
-    f"mysql+aiomysql://{config['MYSQL_USER']}:{config['MYSQL_PASSWORD']}@{config['MYSQL_HOST']}/{config['MYSQL_DATABASE']}")
+    f"mysql+aiomysql://{config['MYSQL_USER']}:{config['MYSQL_PASSWORD']}@{config['MYSQL_HOST']}/"
+    f"{config['MYSQL_DATABASE']}")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 manager = Manager()
 store = manager.dict()
@@ -29,23 +34,35 @@ app.add_middleware(
 
 @app.get("/")
 async def root(request: Request):
+    """
+    Метод обрабатывающий корневой метод
+    :param request: значение реквеста
+    :return: значение счетчика
+    """
     date = datetime.datetime.now().isoformat()
     user_agent = request.headers.get('User-Agent')
     async with SessionLocal() as session:
         await session.execute(
-            f"INSERT INTO counters(datetime, client_info, value) VALUES ('{date}','{user_agent}',{store['count']})")
+            "INSERT INTO counters(datetime, client_info, value) "
+            f"VALUES ('{date}','{user_agent}',{store['count']})")
         await session.commit()
     return store['count']
 
 
 @app.get("/stat")
 async def counter(request: Request):
+    """
+    Метод обрабатывающий путь /stat
+    :param request: значение реквеста
+    :return: значение счетчика + 1
+    """
     store["count"] = store["count"] + 1
     date = datetime.datetime.now().isoformat()
     user_agent = request.headers.get('User-Agent')
     async with SessionLocal() as session:
         await session.execute(
-            f"INSERT INTO counters(datetime, client_info, value) VALUES ('{date}','{user_agent}',{store['count']})")
+            "INSERT INTO counters(datetime, client_info, value) "
+            f"VALUES ('{date}','{user_agent}',{store['count']})")
         await session.commit()
 
     return store["count"]
@@ -53,6 +70,11 @@ async def counter(request: Request):
 
 @app.get("/about")
 async def say_hello():
+    """
+    Метод обрабатывающий путь /about
+    :param request: значение реквеста
+    :return: инфу об учащемся
+    """
     name = "Mikhail Ivanov"
     html = f"<h3>Hello, {name}!</h3>"
     return HTMLResponse(content=html, status_code=200)
@@ -60,6 +82,12 @@ async def say_hello():
 
 @app.on_event("startup")
 async def startup_event():
+    """
+    Метод работающий при запуске приложения
+    делает запрос к субд для восстановления
+    значения счетчика по последней записи
+    из таблицы counters
+    """
     uv_logger = logging.getLogger("uvicorn.access")
     handler = logging.StreamHandler()
     handler.setFormatter(
@@ -70,7 +98,7 @@ async def startup_event():
     uv_logger.addHandler(handler)
     async with SessionLocal() as session:
         res = await session.execute(
-            f"SELECT value FROM counters ORDER BY datetime DESC LIMIT 1")
+            "SELECT value FROM counters ORDER BY datetime DESC LIMIT 1")
         count = res.scalar()
         if not count:
             store["count"] = 0
